d3fdaf7a4301adb6900bb0dfab538fd919fa8979
Nicolò Abrate <nicolo.abrate@polito.it>
2021-03-29 08:14:25 +0200

    BUG some implementation bugs for GPT have been fixed, but test is not passed yet

--- methods/GPT.py
+++ methods/GPT.py
@@ -7,22 +7,21 @@ Description: Class for Generalised Perturbation Theory method.
 """
 from numpy import zeros, dot
 from TEST.phasespace import PhaseSpace
-from TEST.phasespace import interp
 
 
 class GPT():
 
-    def __init__(self, N, geom, unperturbed, pertgeom, perturbed, fwd, adj):
+    def __init__(self, N, geom, unperturbed, pertgeom, perturbed, adj):
 
         self.PertOrder = N
         # check forward and adjoint modes consistency
-        M, M1 = len(fwd.eigvals), len(adj.eigvals)
+        M, M1 = len(unperturbed.eigvals), len(adj.eigvals)
         if M != M1:
             raise OSError('Forward (%d) and adjoint (%d) modes mismatch!' % (M, M1))
         else:
             self.ModExpOrder = M
-            phi, psi =  fwd.eigvect, adj.eigvect
-            mu = fwd.eigvals
+            phi, psi =  unperturbed.eigvect, adj.eigvect
+            mu = 1/unperturbed.eigvals
 
         # define phase space
         PS = PhaseSpace(geom)
@@ -33,78 +32,81 @@ class GPT():
         Ap, Bp = perturbed.A, perturbed.B
 
         # normalisation constants
-        C = zeros(phi.shape[1],1)
+        C = zeros((phi.shape[1], ))
         for i in range(0, self.ModExpOrder):
-            C[i] = PS.braket(psi[:,i], dot(B, psi[:,i]))
+            C[i] = PS.braket(psi[:,i], B.dot(psi[:,i]))
 
         # pre-allocation
-        alpha = zeros((M, N-1))  # alpha_{1,K} = 0 as normalization constant
-        lambdas = zeros((N-1, 1))  # perturbed eigenvalue
-        p = zeros((M, N-1)) # perturbed system flux perturbations
+        alpha = zeros((M, N))  # alpha_{1,K} = 0 as normalization constant
+        lambdas = zeros((N, ))  # perturbed eigenvalue
+        p = zeros((phi.shape[0], N)) # perturbed system flux perturbations
         # normalisation constants
-        tmp = PS.interp(p[:, 0], xp)  # interpolate over pert. mesh
-        tmp = PS.interp(dot(Bp, tmp))  # interpolate product over ref. mesh
+        tmp = PS.interp(phi[:, 0], xp, isref=False)  # interpolate over pert. mesh
+        tmp = PS.interp(Bp.dot(tmp), xp)  # interpolate product over refer. mesh
         Pp = PS.braket(tmp)
 
         # eigenvalue perturbations
-        for n in range(0, self.N):
+        for n in range(0, self.PertOrder):
             if n == 0:
-                v1 = PS.interp(phi[:, 0], xp)
-                S = PS.interp(dot(Ap-mu[0]*Bp, v1))-dot(A-mu[0]*B, phi[:, 0])
+                v1 = PS.interp(phi[:, 0], xp, isref=False)
+                S = PS.interp((Ap-mu[0]*Bp).dot(v1), xp)-(A-mu[0]*B).dot(phi[:, 0])
             else:
-                v1 = PS.interp(p[:, n-1], xp)
-                S = PS.interp(dot(Ap-mu[0]*Bp, v1))-dot(A-mu[0]*B, p[:, n-1])
+                v1 = PS.interp(p[:, n-1], xp, isref=False)
+                S = PS.interp((Ap-mu[0]*Bp).dot(v1), xp)-(A-mu[0]*B).dot(p[:, n-1])
 
             S1, S2 = 0, 0
 
             # sum previous contributions
-            for k in range(0, n-1):
+            for k in range(1, n-1):
                 if n > k:
                     if n > k+1:
-                        v1 = PS.interp(p[:, n-k-1], xp)
+                        v1 = PS.interp(p[:, n-k-1], xp, isref=False)
                         v2 = p[:, n-k-1]
                     else:
-                        v1 = PS.interp(phi[:, 0], xp)
+                        v1 = PS.interp(phi[:, 0], xp, isref=False)
                         v2 = phi[:, 0]
 
-                    S1 = S1+lambdas[k]*dot(B, p[:, n-k])
-                    S2 = S2+lambdas[k]*(PS.interp(dot(Bp, v1))-dot(B, v2))
+                    S1 = S1+lambdas[k]*B.dot(p[:, n-k])
+                    S2 = S2+lambdas[k]*(PS.interp(Bp.dot(v1), xp)-B.dot(v2))
 
             lambdas[n] = (PS.braket(psi[:, 0], S)-PS.braket(psi[:, 0], S1)+
-                         -PS.braket(psi[:, 0], S2))/C[0]
+                          -PS.braket(psi[:, 0], S2))/C[0]
 
             # eigenvector perturbations
             for m in range(1, M):
                 # initialisation 
                 S1a, S2a = 0, 0
-                for k in range(0, n-1):
+                for k in range(1, n-1):
                     if n > k:
                         if n > k+1:
-                            v1 = PS.interp(p[:, n-k-1], xp)
+                            v1 = PS.interp(p[:, n-k-1], xp, isref=False)
                             v2 = p[:, n-k-1]
                         else:
-                            v1 = PS.interp(phi[:, 0], xp)
+                            v1 = PS.interp(phi[:, 0], xp, isref=False)
                             v2 = phi[:, 0]
 
                         S1a = S1a+lambdas[k]*alpha[m, n-k]
-                        S2a = S2a+lambdas[k]*(PS.interp(dot(Bp, v1))-dot(B, v2))
+                        S2a = S2a+lambdas[k]*(PS.interp(Bp.dot(v1), xp)-B.dot(v2))
 
                 # coefficient evaluation
-                alpha[m, n] = (-PS.braket(psi[:, m], S)+S1a*C[m]+
-                               +PS.braket(psi[:, m], S2a))
+                alpha[m, n] = -PS.braket(psi[:, m], S)+S1a*C[m]+PS.braket(psi[:, m], S2a)
                 alpha[m, n] = alpha[m, n]/((mu[m]-mu[0])*C[m])
                 
                 if n == 0:
-                    v1 = PS.interp(phi[:, 0], xp)
-                    Q1 = -PS.braket(PS.interp(dot(Bp, v1))-dot(B, phi[:, 0]))
-                    v1 = PS.interp(dot(phi[:, 1:M], alpha[1:M, n]), xp)
-                    Q2 = PS.braket(PS.interp(Bp*v1))
+                    v1 = PS.interp(phi[:, 0], xp, isref=False)
+                    Q1 = -PS.braket(PS.interp(Bp.dot(v1), xp)-B.dot(phi[:, 0]))
+                    v1 = PS.interp(dot(phi[:, 1:M], alpha[1:M, n]), xp, isref=False)
+                    Q2 = PS.braket(PS.interp(Bp.dot(v1), xp))
                     alpha[0, n] = (Q1+Q2)/Pp
                 else:
-                    v1 = PS.interp(dot(phi[:, 1:M], alpha[1:M, n]), xp)
-                    alpha[0, n] = -PS.braket(PS.interp(Bp*v1))/Pp
+                    v1 = PS.interp(dot(phi[:, 1:M], alpha[1:M, n]), xp, isref=False)
+                    alpha[0, n] = -PS.braket(PS.interp(Bp.dot(v1), xp))/Pp
                 # perturbation reconstruction
                 p[:, n] = dot(phi[:, 0:M], alpha[:, n])
 
         self.EvalPert = lambdas
+        self.ExpCoeff = alpha
         self.EvecPert = p
+        self.pertEigv = mu[0]+lambdas.sum()
+        self.pertEvec = phi[:, 0]+p.sum(axis=1)
+        
--- phasespace.py
+++ phasespace.py
@@ -12,8 +12,10 @@ class PhaseSpace:
 
     def __init__(self, geometry, energygrid=None):
         self.geometry = geometry
+        if energygrid is not None:
+            self.energygrid = energygrid
 
-    def braket(self, v1, v2):
+    def braket(self, v1, v2=None):
         """
         Compute bra-ket product over the phase space.
 
@@ -31,21 +33,62 @@ class PhaseSpace:
         None.
 
         """
-        if self.geometry.nA > 0:
+        if self.geometry.AngOrd > 0:
             raise OSError('GPT cannot be applied yet to transport solutions!')
 
-        v1v2 = np.multiply(v1, v2)
+        v1v2 = np.multiply(v1, v2) if v2 is not None else v1
+            
         G = self.geometry.nE
         S = self.geometry.nS
         grid = self.geometry.mesh
         I = 0
-        # FIXME consider integration over the energy
+        # TODO consider integration over non-group energy grid
         for g in range(0, G):
             skip = g*S
-            I = np.trapz(v1v2[skip:skip+S], x=grid)
+            I = I+np.trapz(v1v2[skip:skip+S], x=grid)
         return I
 
 
-def interp():
-    print()
+    def interp(self, yp, xx, isref=True):
+        """
+        Interpolate linearly vector yp on a different phase space geometrical 
+        grid.
 
+        Parameters
+        ----------
+        yp : ndarray
+            Vector to be interpolated.
+        xx : TYPE
+            Old or new grid according to isref argument.
+        isref : bool, optional
+            Flag for grid to be used in the interpolation. The default is True.
+
+        Raises
+        ------
+        OSError
+            Phase space interpolation cannot be applied yet to transport solutions!
+
+        Returns
+        -------
+        y : ndarray
+            Vector interpolated over the new phase space grid.
+
+        """
+        G = self.geometry.nE
+        if self.geometry.AngOrd > 0:
+            raise OSError('Phase space interpolation cannot be applied yet to transport solutions!')
+
+        if isref is True:
+            x = self.geometry.mesh
+            xp = xx            
+        else:
+            x = xx
+            xp = self.geometry.mesh
+
+        n = len(x)
+        N = len(xp)
+        y = np.zeros((G*N,))
+        for g in range(0, G):
+            y[g*N:(g+1)*N] = np.interp(xp, x, yp[g*n:(g+1)*n])
+
+        return y                       
