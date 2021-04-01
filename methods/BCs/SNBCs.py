BUG SN leakage operator is now consistent with the DD scheme, but more work is needed to fix further bugs

methods/BCs/SNBCs.py
@@ -52,48 +52,48 @@ def setBCs(op, geometry):
         if bc not in ['vacuum', 'mark', 'Mark']:
             raise OSError('Unknown boundary condition {}!'.format(bc))
 
-        for gro in range(0, op.nE):
-            skip = M*N*gro
-            # sweeping to impose BCs
-            for n in range(0, N):
-                if mu[n] > 0:
-                    # no incoming flux
-                    # L[skip, skip+1] = 0
-                    # L[skip, skip] = 1
-                    # op.F[skip, M*np.arange(0, N)] = 0
-                    # op.S[skip, M*np.arange(0, N)] = 0
-                    # op.R[skip, skip] = 0
-                    # if op.state != 'steady':
-                    #     op.T[skip, skip] = 0
-                    L[skip, skip] = 0
-                    op.F[skip, skip] = 0
-                    op.S[skip, skip] = 0
-                    op.R[skip, skip] = 0
-                    if op.state != 'steady':
-                        op.T[skip, skip] = 0
-                    # upwind scheme for left boundary
-                    L[skip+M-1, skip+M-2] = 1/2*L[skip+M-1, skip+M-2]
-                    L[skip+M-1, skip+M-1] = -L[skip+M-1, skip+M-2]                        
-                    skip = skip+M
-                elif  mu[n] < 0: 
-                    # upwind scheme for right boundary
-                    L[skip, skip+1] = 1/2*L[skip, skip+1]
-                    L[skip, skip] = -L[skip, skip+1]
-                    # no incoming flux
-                    # L[skip+M-1, skip+M-1] = 1
-                    # L[skip+M-1, skip+M-2] = 0
-                    # op.S[skip+M-1, M-1+M*np.arange(0, N)] = 0
-                    # op.F[skip+M-1, M-1+M*np.arange(0, N)] = 0
-                    # op.R[skip+M-1, skip+M-1] = 0
-                    # if op.state != 'steady':
-                    #     op.T[skip+M-1, skip+M-1] = 0
-                    L[skip+M-1, skip+M-1] = 0
-                    op.S[skip+M-1, skip+M-1] = 0
-                    op.F[skip+M-1, skip+M-1] = 0
-                    op.R[skip+M-1, skip+M-1] = 0
-                    if op.state != 'steady':
-                        op.T[skip+M-1, skip+M-1] = 0
-                    skip = skip+M
+        # for gro in range(0, op.nE):
+        #     skip = M*N*gro
+        #     # sweeping to impose BCs
+        #     for n in range(0, N):
+        #         if mu[n] > 0:
+        #             # no incoming flux
+        #             # L[skip, skip+1] = 0
+        #             # L[skip, skip] = 1
+        #             # op.F[skip, M*np.arange(0, N)] = 0
+        #             # op.S[skip, M*np.arange(0, N)] = 0
+        #             # op.R[skip, skip] = 0
+        #             # if op.state != 'steady':
+        #             #     op.T[skip, skip] = 0
+        #             L[skip, skip] = 0
+        #             op.F[skip, skip] = 0
+        #             op.S[skip, skip] = 0
+        #             op.R[skip, skip] = 0
+        #             if op.state != 'steady':
+        #                 op.T[skip, skip] = 0
+        #             # upwind scheme for left boundary
+        #             L[skip+M-1, skip+M-2] = 1/2*L[skip+M-1, skip+M-2]
+        #             L[skip+M-1, skip+M-1] = -L[skip+M-1, skip+M-2]                        
+        #             skip = skip+M
+        #         elif  mu[n] < 0: 
+        #             # upwind scheme for right boundary
+        #             L[skip, skip+1] = 1/2*L[skip, skip+1]
+        #             L[skip, skip] = -L[skip, skip+1]
+        #             # no incoming flux
+        #             # L[skip+M-1, skip+M-1] = 1
+        #             # L[skip+M-1, skip+M-2] = 0
+        #             # op.S[skip+M-1, M-1+M*np.arange(0, N)] = 0
+        #             # op.F[skip+M-1, M-1+M*np.arange(0, N)] = 0
+        #             # op.R[skip+M-1, skip+M-1] = 0
+        #             # if op.state != 'steady':
+        #             #     op.T[skip+M-1, skip+M-1] = 0
+        #             L[skip+M-1, skip+M-1] = 0
+        #             op.S[skip+M-1, skip+M-1] = 0
+        #             op.F[skip+M-1, skip+M-1] = 0
+        #             op.R[skip+M-1, skip+M-1] = 0
+        #             if op.state != 'steady':
+        #                 op.T[skip+M-1, skip+M-1] = 0
+        #             skip = skip+M
 
     op.L = L
     return op

methods/angle/discreteordinates/SN.py
@@ -103,19 +103,47 @@ def leakage(obj, fmt='csc'):
     M = []
     appM = M.append
     mu = obj.QW['mu']
+
+    # --- create sub-matrix
+
+    # fill lower triangular matrix (mu > 0)
+    tmp = []
+    for i in range(0, obj.nS):
+        coeff = 2/obj.dx
+        if i == 0:
+            if model == 'FD':
+                d = FD.zero(obj, coeff).T.flatten()
+            elif model == 'FV':
+                d = FV.zero(obj, coeff).T.flatten()
+            else:
+                raise OSError('{} model not available for spatial variable!'.format(model))
+            tmp.append(list(d))
+        elif i % 2 != 0:
+            tmp.append(list(-2*d[0+i:obj.nS]))
+        else:
+            tmp.append(list(2*d[0+i:obj.nS]))
+    trilpos = diags(tmp, np.arange(0, -obj.nS, -1), (obj.nS, obj.nS), format=fmt)
+    # fill lower triangular matrix (mu < 0)
+    tmp = []
+    for i in range(0, obj.nS):
+        coeff = 2/np.flipud(obj.dx)
+        if i == 0:
+            if model == 'FD':
+                d = FD.zero(obj, coeff).T.flatten()
+            elif model == 'FV':
+                d = FV.zero(obj, coeff).T.flatten()
+            else:
+                raise OSError('{} model not available for spatial variable!'.format(model))
+            tmp.append(list(d))
+        elif i % 2 != 0:
+            tmp.append(list(2*d[0+i:obj.nS]))
+        else:
+            tmp.append(list(-2*d[0+i:obj.nS]))
+    trilneg = diags(tmp, np.arange(0, -obj.nS, -1), (obj.nS, obj.nS), format=fmt)
+
     for order in range(0, N):
 
-        if model == 'FD':
-            d = FD.first(obj, mu[order], stag=False)
-        elif model == 'FV':
-            d = FV.first(obj, mu[order], stag=False)
-        else:
-            raise OSError('{} model not available for spatial variable!'.format(model))
-
-        n = d.shape[1]
-        pos = np.array([-1, 1])
-        mat = diags(d, pos, (n, n), format=fmt)
-
+        mat = mu[order]*trilpos if mu[order] >= 0 else mu[order]*trilneg
         appM(mat)
 
     M = block_diag((M))