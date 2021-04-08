c65f197ab8308eda45e570149ffa517f24f80b1b
Nicolo Abrate <nicolo.abrate@polito.it>
2021-04-08 18:56:06 +0200

    DEV work in progress for SN in case of source-driven problems

--- tests/test_source1G.py
+++ tests/test_source1G.py
@@ -15,11 +15,11 @@ from TEST.models.SourceProblem import sourceproblem
 import matplotlib.pyplot as plt
 
 nev = 1
-M = 100
+M = -5
 N = 1
 G = 1
 bc = 'Mark'
-H = 10
+H = 1
 xlayers = [0, H]
 # define geometry and mesh
 myslab = Slab(M, xlayers, ['tmp2'], [bc], G, N, 'FD')
@@ -55,8 +55,50 @@ for x in np.linspace(0, H/2, 1000):
 # Q[0:myslab2.nS] = 1
 phiSN = sS.solution.flux
 totphiSN = phiSN[0:myslab2.nS]+np.flipud(phiSN[myslab2.nS:2*myslab2.nS])
+
+phi1 = lambda x: 20*(S0/(1*2)*(1-np.exp(-np.sqrt(3)*x)))
+phi2 = lambda x: 20*(S0/(1*2)*(1-np.exp(-np.sqrt(3)*(H-x))))
+# phi2 = lambda x: 20*(S0/(1*2)*(np.exp(np.sqrt(3)*x)-np.exp(np.sqrt(3)*(2*x-H))))
+phiT = lambda x: phi1(x)+phi2(x)
+
+plt.plot(myslab.mesh, phiSN[0:myslab2.nS], label='phi1 SN')
+plt.plot(myslab.mesh, phi1(myslab.mesh), label='phi1 exact')
+plt.plot(myslab.mesh, np.flipud(phi1(myslab.mesh)), label='phi1 flipped')
+plt.plot(myslab.mesh, phi2(myslab.mesh), label='phi2 exact')
+plt.legend()
+plt.show()
+
+plt.plot(myslab.mesh, np.flipud(phi1(myslab.mesh))+phi1(myslab.mesh), label='phitot')
+# plt.plot(myslab.mesh, np.flipud(phiSN[myslab2.nS:2*myslab2.nS]))
+plt.legend()
+plt.show()
+
 # plt.plot(myslabD.mesh, sD.solution.flux, label='Diff')
-plt.plot(myslab.mesh, sP.solution.flux[0:myslab.nS], label='PN')
+plt.plot(myslab.mesh, sP.solution.flux[0:myslab.nS], label='PN', c='r')
 plt.plot(myslab2.mesh, totphiSN, label='SN')
+plt.plot(myslab.mesh, phiT(myslab.mesh), '--', label='exact', c='b')
 # plt.plot(np.linspace(0, H/2, 1000), tmp, label='Diff. analytical')
 plt.legend()
+plt.show()
+
+dx = myslab.dx[0]
+phi1_num = np.zeros((myslab.nS, ))
+phi2_num = np.zeros((myslab.nS, ))
+q = (S0/2)*20
+mu = 1/np.sqrt(3)
+for i in range(1, myslab.nS):
+    phi1_num[i] = q/(mu/dx+1/2)+phi1_num[i-1]*(1-dx/(2*mu))/(1+dx/(2*mu))
+    phi2_num[myslab.nS-i-1] = q/(mu/dx+1/2)+phi2_num[myslab.nS-i]*(1-dx/(2*mu))/(1+dx/(2*mu))
+
+
+plt.plot(myslab.mesh, phiSN[0:myslab2.nS], label='phi1 SN')
+plt.plot(myslab.mesh, phi1(myslab.mesh), label='phi1 exact')
+plt.plot(myslab.mesh, phi1_num, label='phi1 SN+FD')
+plt.legend()
+plt.show()
+
+plt.plot(myslab.mesh, np.flipud(phiSN[myslab2.nS:2*myslab2.nS]), label='phi2 SN')
+plt.plot(myslab.mesh, phi2(myslab.mesh), label='phi2 exact')
+plt.plot(myslab.mesh, phi2_num, label='phi2 SN+FD')
+plt.legend()
+plt.show()
