4d6be7f6e9f1b65605726e4e9f14b2e5c5eac485
nicolo.abrate <nicolo.abrate@polito.it>
2021-11-02 17:15:20 +0100

    DEV added energy grid reader

---
+++ utils/utils.py
@@ -0,0 +1,19 @@
+"""
+Author: N. Abrate.
+
+File: get_energy_grid.py
+
+Description: Utility to get default energy grid stored in
+             ../material/datalib/group_structures.
+"""
+import numpy as np
+from pathlib import Path
+
+def get_energy_grid(grid_name):
+    pwd = Path(__file__).parent.parent
+    pwd = pwd.joinpath('material')
+    egridpath = pwd.joinpath('datalib', 'group_structures',
+                             '{}.txt'.format(grid_name))
+    energygrid = np.loadtxt(egridpath)
+    energygrid.sort()
+    return energygrid
